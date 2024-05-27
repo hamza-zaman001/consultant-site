f9b1d29c71c860ebaeef0b915e021384cb5bf074
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2024-05-27 21:03:10 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Function to handle the fade-in effect
+window.addEventListener('load', () => {
+    document.querySelectorAll('.intro, .solutions').forEach(element => {
+        element.style.opacity = '1';
+    });
+});
